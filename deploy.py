b1481d79b28dea7367abc5268f69c47f59b26036
Prince Andrew <prandrew582@yahoo.com>
2022-08-27 13:36:26 -0600

    Tested and approved by the client

--- deploy.py
+++ deploy.py
@@ -3,4 +3,6 @@
 # echo "Hello World"  #shell scripts    
 print('Hello World') # python script
 print('Deploy to dev environment') 
+print('Excellent job')
+print('Tested and accepted')
 # I love python language
