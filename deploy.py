1b7a38bb299e2ceb36e7acfd6251ce28d794bb26
Prince Andrew <prandrew582@yahoo.com>
2022-08-27 13:33:19 -0600

    Deployment to the dev environment

--- deploy.py
+++ deploy.py
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Deploying a python web application
 # echo "Hello World"  #shell scripts    
-print('Hello World') # python script 
+print('Hello World') # python script
+print('Deploy to dev environment') 
 # I love python language
