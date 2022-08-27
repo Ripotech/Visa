32b568eb5c51de0b98aac4bad95f87f3ebf13e72
Prince Andrew <prandrew582@yahoo.com>
2022-08-27 13:39:43 -0600

    Client is happy

--- deploy.py
+++ deploy.py
@@ -5,4 +5,5 @@ print('Hello World') # python script
 print('Deploy to dev environment') 
 print('Excellent job')
 print('Tested and accepted')
+print('The Client is very happy with the team!!')
 # I love python language
